5444643410ab9b329a8f9ea0607087bb49417e6d
ewitmac <maciej.witkowski@ericsson.com>
2024-08-30 11:19:25 +0200

    update python plot script

--- PYTH/main.py
+++ PYTH/main.py
@@ -1,71 +1,33 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from pandas.plotting import table
+import seaborn as sns
 
-df = pd.read_csv('../data/data.txt', sep=';')
-df = df.sort_values(by=['entropy'])
-df.index = [''] * len(df)
-rows, columns = df.shape
+df = pd.read_csv('../data/data.txt', delimiter=';')
+df['dist_comp'] = df['distribution'] + ' | ' + df['compression']
 
-fig, ax = plt.subplots(figsize=(20, 10))
-ax.axis('off') 
-tbl = table(ax, df, loc='center', cellLoc='center', colWidths=[0.11]*len(df.columns))
-tbl.auto_set_font_size(False)
-tbl.set_fontsize(12)
-tbl.scale(1.2, 1.2)
+sns.set(style="darkgrid")
 
-for (i, j), cell in tbl.get_celld().items():
-    if i == 0:
-        cell.set_text_props(weight='bold')
-    cell.set_text_props(ha='center', va='center')
-    cell.set_edgecolor('black')
-    cell.set_linewidth(1)
+custom_colors = ['#ff2d00', '#941a00', '#00ffdb', '#00947f']
 
-plt.savefig('../data/data.png', bbox_inches='tight', dpi=300)
-plt.close()
+plt.figure(figsize=(12, 10))
 
-exp_df = df[df['distribution'] == 'exponential']
+ax = sns.barplot(data=df, x='rounding_type', y='entropy', hue='dist_comp', palette=custom_colors)
 
-exp_df_compressed = exp_df[exp_df['compression'] == 'Z_BEST_COMPRESSION']
+for p in ax.patches:
+    height = p.get_height()
+    ax.annotate(format(height, '.2f'),
+                (0.01 + p.get_x() + p.get_width() / 2., height - 0.5),
+                ha='center', va='bottom',
+                xytext=(0, 5),
+                textcoords='offset points',
+                fontsize=13,
+                rotation=90,
+                color='white',fontweight='bold')
 
-exp_df_not_compressed = exp_df[exp_df['compression'] == 'NO']
-
-
-uni_df = df[df['distribution'] == 'uniform']
-
-uni_df_compressed = uni_df[uni_df['compression'] == 'Z_BEST_COMPRESSION']
-
-uni_df_not_compressed = uni_df[uni_df['compression'] == 'NO']
-
-
-ax = uni_df_compressed.plot.bar(x='rounding_type', y='entropy', legend=True, color='skyblue')
-ax.set_xlabel('rounding type')
-ax.set_ylabel('entropy')
-ax.set_title('Entropy for uniform distribution with compression')
-ax.grid(True)
-plt.savefig('../data/uni_compression.png', bbox_inches='tight', dpi=300)
-plt.close()
-
-ax = uni_df_not_compressed.plot.bar(x='rounding_type', y='entropy', legend=True, color='skyblue')
-ax.set_xlabel('rounding type')
-ax.set_ylabel('entropy')
-ax.set_title('Entropy for uniform distribution without compression')
-ax.grid(True)
-plt.savefig('../data/uni_no_compression.png', bbox_inches='tight', dpi=300)
-plt.close()
-
-ax = exp_df_compressed.plot.bar(x='rounding_type', y='entropy', legend=True, color='skyblue')
-ax.set_xlabel('rounding type')
-ax.set_ylabel('entropy')
-ax.set_title('Entropy for exponential distribution with compression')
-ax.grid(True)
-plt.savefig('../data/exp_compression.png', bbox_inches='tight', dpi=300)
-plt.close()
-
-ax = uni_df_not_compressed.plot.bar(x='rounding_type', y='entropy', legend=True, color='skyblue')
-ax.set_xlabel('rounding type')
-ax.set_ylabel('entropy')
-ax.set_title('Entropy for exponential distribution without compression')
-ax.grid(True)
-plt.savefig('../data/exp_no_compression.png', bbox_inches='tight', dpi=300) 
-plt.close()
+plt.title('Entropy value for different rounding methods, distributions and compression', fontsize=18, fontweight='bold')
+plt.xlabel('Rounding type')
+plt.ylabel('Entropy [bit]')
+plt.legend(title='Distribution | Compression', bbox_to_anchor=(1, 0.85), loc='center right', fontsize=13)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.savefig('../data/data.png')  # Save the figure to a file (e.g., 'entropy_plot.png')
